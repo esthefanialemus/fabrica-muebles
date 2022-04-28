3a34161e25761fbd7af09c880922c317a5297001
Esthefanía Lemus Preciado - Ceiba Software <esthefania.lemus@ceiba.com.co>
2022-04-28 14:08:32 -0500

    implementacion test usuario intento 2

--- microservicio/infraestructura/src/test/java/com/ceiba/controlador/usuario/ComandoControladorUsuarioTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/controlador/usuario/ComandoControladorUsuarioTest.java
@@ -1,29 +1,27 @@
-package com.ceiba.controlador;
+package com.ceiba.controlador.usuario;
 
 import com.ceiba.ApplicationMock;
-import com.ceiba.servicio.testdatabuilder.ComandoUsuarioTestDataBuilder;
 import com.ceiba.comando.ComandoUsuario;
+import com.ceiba.controlador.ComandoControladorUsuario;
+import com.ceiba.servicio.testdatabuilder.ComandoUsuarioTestDataBuilder;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest(ComandoControladorUsuario.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration(classes= ApplicationMock.class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
-class ComandoControladorUsuarioTest {
+@WebMvcTest(ComandoControladorUsuario.class)
+public class ComandoControladorUsuarioTest {
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -32,9 +30,10 @@ class ComandoControladorUsuarioTest {
     private MockMvc mocMvc;
 
     @Test
-    void crear() throws Exception{
+    public void crear() throws Exception{
         // arrange
         ComandoUsuario usuario = new ComandoUsuarioTestDataBuilder().build();
+
         // act - assert
         mocMvc.perform(post("/usuario")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -44,10 +43,11 @@ class ComandoControladorUsuarioTest {
     }
 
     @Test
-    void actualizar() throws Exception{
+    public void actualizar() throws Exception{
         // arrange
         Long id = 2L;
         ComandoUsuario usuario = new ComandoUsuarioTestDataBuilder().build();
+
         // act - assert
         mocMvc.perform(put("/usuario/{id}",id)
                 .contentType(MediaType.APPLICATION_JSON)
@@ -56,15 +56,14 @@ class ComandoControladorUsuarioTest {
     }
 
     @Test
-    void eliminar() throws Exception {
+    public void eliminar() throws Exception {
         // arrange
         Long id = 2L;
+
         // act - assert
         mocMvc.perform(delete("/usuario/{id}",id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
-
     }
-
 }
