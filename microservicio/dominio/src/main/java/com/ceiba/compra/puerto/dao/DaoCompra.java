993cb3ad4c795e751fac1aefdad244e2f7bd9453
Esthefanía Lemus Preciado - Ceiba Software <esthefania.lemus@ceiba.com.co>
2022-04-28 15:59:13 -0500

    integracion contninua parte 2

--- microservicio/dominio/src/main/java/com/ceiba/compra/puerto/dao/DaoCompra.java
+++ microservicio/dominio/src/main/java/com/ceiba/compra/puerto/dao/DaoCompra.java
@@ -2,21 +2,7 @@ package com.ceiba.compra.puerto.dao;
 
 import com.ceiba.compra.modelo.dto.DtoCompra;
 
-import java.util.List;
-
 public interface DaoCompra {
 
-    /**
-     * Permite listar Compras
-     * @return Las Compras
-     */
-    List<DtoCompra> listar();
-
-    /**
-     * Permite obtener una Compra dado su id
-     *
-     * @param id
-     * @return la Compra
-     */
-    DtoCompra obtener(Long id);
+    public DtoCompra listarCompraPorId (Long idCompra);
 }
