7b5ed045f9d38aa0fd2b5a68fea7fd5db7b8ce6a
Esthefanía Lemus Preciado - Ceiba Software <esthefania.lemus@ceiba.com.co>
2022-05-18 16:10:36 -0500

    dto items compra

--- microservicio/aplicacion/src/main/java/com/ceiba/consulta/itemscompra/ManejadorListarDaoItemsCompra.java
+++ microservicio/aplicacion/src/main/java/com/ceiba/consulta/itemscompra/ManejadorListarDaoItemsCompra.java
@@ -1,7 +1,6 @@
 
 package com.ceiba.consulta.itemscompra;
 
-import com.ceiba.items_compra.modelo.dto.DtoItemsCompra;
 import com.ceiba.items_compra.modelo.dto.DtoItemsCompraConsulta;
 import com.ceiba.items_compra.puerto.dao.DaoItemsCompra;
 import org.springframework.stereotype.Component;
